914b6ab155ab1d31565455d47bc3a33f0e6a1d1e
Clayton Charles <claydevfull@gmail.com>
2026-03-18 12:59:30 -0300

    feat: implementação de um favicon e logo personalizado (simples)

--- resources/js/components/default/app-logo.tsx
+++ resources/js/components/default/app-logo.tsx
@@ -4,11 +4,11 @@ import AppLogoIcon from "../icons/app-logo-icon";
 export default function AppLogo() {
   return (
     <>
-      <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-        <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+      <div className="flex aspect-square size-8 items-center justify-center rounded-md">
+        <AppLogoIcon />
       </div>
       <div className="ml-1 grid flex-1 text-left text-sm">
-        <span className="mb-0.5 truncate leading-tight font-semibold">
+        <span className="mb-0.5 truncate leading-tight font-semibold text-2xl">
           Trackio
         </span>
       </div>
--- resources/js/layouts/simple-layout.tsx
+++ resources/js/layouts/simple-layout.tsx
@@ -16,8 +16,8 @@ export default function SimpleLayout({
               href="#"
               className="flex flex-col items-center gap-2 font-medium"
             >
-              <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-9 fill-current text-foreground dark:text-white" />
+              <div className="mb-1 flex size-14 items-center justify-center rounded-md">
+                <AppLogoIcon />
               </div>
               <span className="sr-only">{title}</span>
             </Link>
